f0b90b87c7f4d026c13c2c64344e62d500900ca8
Solidwave <jamesfrancario@gmail.com>
2023-01-21 23:55:17 +0100

    feat: adds level selection

---
+++ LevelButton.gd
@@ -0,0 +1,21 @@
+extends Control
+
+class_name LevelButton
+
+@export var levelName : String
+
+@export var levelPath : String
+
+@onready var button : Button = $Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	button.text = levelName
+	
+	print(levelPath)
+
+
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file(levelPath)
--- Scripts/control.gd
+++ Scripts/control.gd
@@ -28,3 +28,9 @@ func _on_options_button_pressed():
 func _on_quit_button_pressed():
 	get_tree().quit()
 	pass # Replace with function body.
+
+
+func _on_levels_pressed():
+	get_tree().change_scene_to_file("res://levels_ui.tscn")
+	
+	pass # Replace with function body.
